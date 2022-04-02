266609e28d00ab575f1fc97f5a0bcb8d4dea476d
Arron <beale86@msn.com>
2022-04-02 13:43:07 +0000

    Add function names for game

--- assets/js/game.js
+++ assets/js/game.js
@@ -58,30 +58,74 @@ let words = [
 ];
 
 let alphabet = [
-    a,
-    b,
-    c,
-    d,
-    e,
-    f,
-    g,
-    h,
-    i,
-    j,
-    k,
-    l,
-    m,
-    n,
-    o,
-    p,
-    q,
-    r,
-    s,
-    t,
-    u,
-    v, 
-    w,
-    x,
-    y,
-    z
+    'a',
+    'b',
+    'c',
+    'd',
+    'e',
+    'f',
+    'g',
+    'h',
+    'i',
+    'j',
+    'k',
+    'l',
+    'm',
+    'n',
+    'o',
+    'p',
+    'q',
+    'r',
+    's',
+    't',
+    'u',
+    'v', 
+    'w',
+    'x',
+    'y',
+    'z'
 ];
+
+function generateAnswer() {
+
+}
+
+function checkCorrect() {
+
+} 
+
+function startGame() {
+
+}
+
+function stopGame() {
+
+}
+
+function countDown() {
+
+}
+
+function easyMode() {
+
+}
+
+function easyModeCountdown() {
+
+}
+
+function mediumMode() {
+    
+}
+
+function mediumModeCountDown() {
+    
+}
+
+function hardMode() {
+    
+}
+
+function hardModeCountDown() {
+    
+}
