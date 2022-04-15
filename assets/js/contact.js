de8ee1b2c400eccfb0bdb16ae2cc80323a6ec6e4
Arron <beale86@msn.com>
2022-04-15 13:27:31 +0000

    Add function to validate form to prevent submission with blank fields

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -1,6 +1,29 @@
 let submitBtn = document.getElementById('submit');
 submitBtn.addEventListener('click', handleSubmit);
 
+let valid = false;
+
+function validateForm() {
+    let x = document.forms["contact-form"]["name"].value;
+    let y = document.forms["contact-form"]["email"].value;
+    let z = document.forms["contact-form"]["message"].value;
+    if (x == "") {
+        valid = false;
+    } else {
+        valid = true;
+    }
+    if (y == "") {
+        valid = false;
+    } else {
+        valid = true;
+    }
+    if (z == "") {
+        valid = false;
+    } else {
+        valid = true;
+    }
+}
+
 function handleSubmit(event) {
     let contactForm = document.getElementById('contact-form');
     let name = contactForm.elements['name'].value;
@@ -8,10 +31,12 @@ function handleSubmit(event) {
     let message = contactForm.elements['message'].value;
     let html = `
     <h2>Hi ${name}!</h2><br>
-    <p></p>Thanks for sending me a message!</p><br>
-    <p>I will respond to ${email} soon</p><br>
+    <p></p>Thanks for sending me a message.</p><br>
+    <p>I will respond to ${email} soon.</p><br>
     <p>Your message was: ${message}</p>
     `
-    event.preventDefault();
-    contactForm.innerHTML = html;
+    validateForm();
+    if (valid ==true) {setTimeout(function () {
+        contactForm.innerHTML = html;
+    }, 1000);}
 }
