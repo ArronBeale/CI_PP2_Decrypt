ca3cca1890a2db235dbf6afb4218ce1dd36f8f38
Arron <beale86@msn.com>
2022-04-15 13:01:32 +0000

    Add response message to userafter they submit contact message

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -1,12 +1,17 @@
-const submitBtn = document.getElementById('submit');
-const contactForm = document.getElementById('contact-form');
-let name = document.getElementById('user-name');
-
-submitBtn.addEventListener('click', handleSubmit );
+let submitBtn = document.getElementById('submit');
+submitBtn.addEventListener('click', handleSubmit);
 
 function handleSubmit(event) {
+    let contactForm = document.getElementById('contact-form');
+    let name = contactForm.elements['name'].value;
+    let email = contactForm.elements['email'].value;
+    let message = contactForm.elements['message'].value;
+    let html = `
+    <h2>Hi ${name}!</h2><br>
+    <p></p>Thanks for sending me a message!</p><br>
+    <p>I will respond to ${email} soon</p><br>
+    <p>Your message was: ${message}</p>
+    `
     event.preventDefault();
-    contactForm.submit();
-    contactForm.reset();
-    contactForm.innerHTML= `<h2>Thank you for getting in contact ${name}!</h2>`
+    contactForm.innerHTML = html;
 }
