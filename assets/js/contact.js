8cdd54acfb9fc189318d1855e7aaf5aec1ff70a9
Arron <beale86@msn.com>
2022-04-27 18:12:11 +0000

    Move emailjs scripts from contact.html to contact.js, add h2 heading to message box of contact form

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -1,3 +1,26 @@
+/**
+ * this is the template from emailjs to send the form to my Gmail.
+ * service created to allow players to submit feedback or get in touch with me.
+ * more info on emailjs (https://www.emailjs.com/)
+ */
+(function () {
+    // https://dashboard.emailjs.com/admin/account
+    emailjs.init('mcd7Qd9Dhv9KI6qxp');
+})();
+
+window.onload = function () {
+    document.getElementById('contact-form').addEventListener('submit', function (event) {
+        event.preventDefault();
+        // these IDs from the previous steps
+        emailjs.sendForm('contact_service', 'contact_form', this)
+            .then(function () {
+                console.log('SUCCESS!');
+            }, function (error) {
+                console.log('FAILED...', error);
+            });
+    });
+}
+
 let submitBtn = document.getElementById('submit');
  submitBtn.addEventListener('click', handleSubmit);
 
@@ -45,7 +68,8 @@ let submitBtn = document.getElementById('submit');
     <h2>Hi ${name}!</h2><br>
     <p></p>Thanks for sending me a message.</p><br>
     <p>I will respond to ${email} soon.</p><br>
-    <p>Your message was: ${message}</p>
+    <h2>Your message was:</h2><br>
+    <p>${message}</p>
     `;
      validateForm();
      if (a && b && c === true) {
