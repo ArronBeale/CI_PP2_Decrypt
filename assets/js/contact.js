30e676ea50a96d073d6c765ea9854fee0ecabedc
Arron <beale86@msn.com>
2022-04-27 18:20:44 +0000

    Add comments and re word previous comment

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -1,6 +1,6 @@
 /**
  * this is the template from emailjs to send the form to my Gmail.
- * service created to allow players to submit feedback or get in touch with me.
+ * service is created to allow site visitors to submit feedback or get in touch with me.
  * more info on emailjs (https://www.emailjs.com/)
  */
 (function () {
@@ -22,59 +22,60 @@ window.onload = function () {
 }
 
 let submitBtn = document.getElementById('submit');
- submitBtn.addEventListener('click', handleSubmit);
+submitBtn.addEventListener('click', handleSubmit);
 
- let a = false;
- let b = false;
- let c = false;
- let valid = false;
+let a = false;
+let b = false;
+let c = false;
+let valid = false;
 
- function validateForm() {
-     let x = document.getElementById('name').value;
-     let y = document.getElementById('email').value;
-     let z = document.getElementById('message').value;
+/* this function will check all inputs have received data before it will allow a submission */
+function validateForm() {
+    let x = document.getElementById('name').value;
+    let y = document.getElementById('email').value;
+    let z = document.getElementById('message').value;
 
-     if (x === "") {
-         valid = false;
-     } else {
-         a = true;
-     }
-     if (y === "") {
-         valid = false;
-     } else {
-         b = true;
-     }
-     if (z === "") {
-         valid = false;
-     } else {
-         c = true;
-     }
- }
+    if (x === "") {
+        valid = false;
+    } else {
+        a = true;
+    }
+    if (y === "") {
+        valid = false;
+    } else {
+        b = true;
+    }
+    if (z === "") {
+        valid = false;
+    } else {
+        c = true;
+    }
+}
 
- const logoH1 = document.getElementById('logo-h1');
- logoH1.addEventListener('click', function () {
-     window.location = "index.html";
- });
+const logoH1 = document.getElementById('logo-h1');
+logoH1.addEventListener('click', function () {
+    window.location = "index.html";
+});
 
- /** the below code handles the contact form and validates it
-  * to ensure all fields are filled before submission
-  */
- function handleSubmit(event) {
-     let contactForm = document.getElementById('contact-form');
-     let name = document.getElementById('name').value;
-     let email = document.getElementById('email').value;
-     let message = document.getElementById('message').value;
-     let html = `
+/** the below code handles the contact form and validates it
+ * to ensure all fields are filled before submission
+ */
+function handleSubmit(event) {
+    let contactForm = document.getElementById('contact-form');
+    let name = document.getElementById('name').value;
+    let email = document.getElementById('email').value;
+    let message = document.getElementById('message').value;
+    let html = `
     <h2>Hi ${name}!</h2><br>
     <p></p>Thanks for sending me a message.</p><br>
     <p>I will respond to ${email} soon.</p><br>
     <h2>Your message was:</h2><br>
     <p>${message}</p>
     `;
-     validateForm();
-     if (a && b && c === true) {
-         setTimeout(function () {
-             contactForm.innerHTML = html;
-         }, 1000);
-     }
- }
+    validateForm();
+    if (a && b && c === true) {
+        setTimeout(function () {
+            contactForm.innerHTML = html;
+        }, 1000);
+    }
+}
